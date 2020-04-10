10d9bbcf56a2f7730e8bba36e56d5dd4dd1798c5
Diego Rodriguez Paiva <diegorodriguez93@hotmail.com>
2020-04-10 20:29:35 -0300

    cors y body parser

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var ChessWebAPI = require('chess-web-api');
 
+app.use(function(req,res, next) {
+    res.header("Access-Control-Allow-Origin","*"); 
+    // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept"); 
+    next();
+    
+    });
+    
+    // for parsing application/json
+    app.use(bodyParser.json());
+    // for parsing application/xwww-
+    app.use(bodyParser.urlencoded({extended:true })); 
+    
+
 var chessAPI = new ChessWebAPI();
+
 
 /* chessAPI.getPlayer('andyruwruw')
     .then(function(res) {
